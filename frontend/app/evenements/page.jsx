3069c7cbc6d7fa65d4248ff51bfb35235e060f43
Valentin Monteiro <valentin.monteiro.pro@gmail.com>
2024-10-04 00:42:41 +0200

    Adapter liste des marchés aux écrans pc

--- frontend/app/evenements/page.jsx
+++ frontend/app/evenements/page.jsx
@@ -33,30 +33,35 @@ export default async function page() {
       {locations && (
         <section className={styles.locations}>
           <h2>Les marchés</h2>
-          {locations.map((location, index) => (
-            <article key={index}>
-              <Image
-                className={styles.photo}
-                src={`http://localhost:8080/api/images/${location.photo}`}
-                width={1024}
-                height={576}
-                alt={`Photo de ${location.name}`}
-              />
-              <div className={styles.filter}></div>
-              <div className={styles.name}>
-                <FontAwesomeIcon className={styles.icon} icon={faLocationDot} />
-                <h3>{location.name}</h3>
-              </div>
-              <div className={styles.frequency}>
-                <FontAwesomeIcon className={styles.icon} icon={faClock} />
-                <p>{location.frequency}</p>
-              </div>
-              <div className={styles.schedule}>
-                <FontAwesomeIcon className={styles.icon} icon={faCalendar} />
-                <p>{location.schedule}</p>
-              </div>
-            </article>
-          ))}
+          <div className={styles.articlesList}>
+            {locations.map((location, index) => (
+              <article key={index}>
+                <Image
+                  className={styles.photo}
+                  src={`http://localhost:8080/api/images/${location.photo}`}
+                  width={1024}
+                  height={576}
+                  alt={`Photo de ${location.name}`}
+                />
+                <div className={styles.filter}></div>
+                <div className={styles.name}>
+                  <FontAwesomeIcon
+                    className={styles.icon}
+                    icon={faLocationDot}
+                  />
+                  <h3>{location.name}</h3>
+                </div>
+                <div className={styles.frequency}>
+                  <FontAwesomeIcon className={styles.icon} icon={faClock} />
+                  <p>{location.frequency}</p>
+                </div>
+                <div className={styles.schedule}>
+                  <FontAwesomeIcon className={styles.icon} icon={faCalendar} />
+                  <p>{location.schedule}</p>
+                </div>
+              </article>
+            ))}
+          </div>
         </section>
       )}
     </main>
