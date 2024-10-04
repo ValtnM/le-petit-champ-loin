f26cf1a7be7a186d8fb5b5da520135d836aab867
Valentin Monteiro <valentin.monteiro.pro@gmail.com>
2024-10-04 23:15:56 +0200

    Adaptation de la page contact aux écrans pc

--- frontend/app/contact/page.jsx
+++ frontend/app/contact/page.jsx
@@ -6,51 +6,52 @@ export default function page() {
   return (
     <main className={styles.contact}>
       <h2>Nous contacter</h2>
-      <section className={styles.phone}>
-        <div className={styles.title}>
-          <FontAwesomeIcon className={styles.icon} icon={faPhone} />
-          <h3>Appelez-nous</h3>
-        </div>
-        <p>01 23 45 67 89</p>
-      </section>
+
       <section className={styles.email}>
         <div className={styles.title}>
           <FontAwesomeIcon className={styles.icon} icon={faEnvelope} />
           <h3>Écrivez-nous</h3>
         </div>
         <form>
-            <div className={styles.gender}>
-                <div className={styles.genderInput}>
-                    <input type="radio" id="woman" name="gender"/>
-                    <label htmlFor="woman">Madame</label>
-                </div>
-                <div className={styles.genderInput}>
-                    <input type="radio" id="man" name="gender" />
-                    <label htmlFor="man">Monsieur</label>
-                </div>
+          <div className={styles.gender}>
+            <div className={styles.genderInput}>
+              <input type="radio" id="woman" name="gender" />
+              <label htmlFor="woman">Madame</label>
             </div>
-            <div className={styles.firstnameInput}>
-                <label htmlFor="firstname">Prénom</label>
-                <input type="text" id="firstname" />
+            <div className={styles.genderInput}>
+              <input type="radio" id="man" name="gender" />
+              <label htmlFor="man">Monsieur</label>
             </div>
-            <div className={styles.lastnameInput}>
-                <label htmlFor="lastname">Nom</label>
-                <input type="text" id="lastname" />
-            </div>
-            <div className={styles.emailInput}>
-                <label htmlFor="email">Email</label>
-                <input type="email" id="email" />
-            </div>
-            <div className={styles.phoneInput}>
-                <label htmlFor="phone">Téléphone</label>
-                <input type="text" id="phone" />
-            </div>
-            <div className={styles.messageInput}>
-                <label htmlFor="message">Message</label>
-                <textarea id="message" rows="10"/>
-            </div>
-            <button>Envoyer</button>
+          </div>
+          <div className={styles.firstnameInput}>
+            <label htmlFor="firstname">Prénom</label>
+            <input type="text" id="firstname" />
+          </div>
+          <div className={styles.lastnameInput}>
+            <label htmlFor="lastname">Nom</label>
+            <input type="text" id="lastname" />
+          </div>
+          <div className={styles.emailInput}>
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" />
+          </div>
+          <div className={styles.phoneInput}>
+            <label htmlFor="phone">Téléphone</label>
+            <input type="text" id="phone" />
+          </div>
+          <div className={styles.messageInput}>
+            <label htmlFor="message">Message</label>
+            <textarea id="message" rows="10" />
+          </div>
+          <button>Envoyer</button>
         </form>
+      </section>
+      <section className={styles.phone}>
+        <div className={styles.title}>
+          <FontAwesomeIcon className={styles.icon} icon={faPhone} />
+          <h3>Appelez-nous</h3>
+        </div>
+        <p>01 23 45 67 89</p>
       </section>
     </main>
   );
