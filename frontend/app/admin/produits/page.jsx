08b5bd5670232d819d96487fe721fb59fa2c530d
Valentin Monteiro <valentin.monteiro.pro@gmail.com>
2024-10-07 19:22:01 +0200

    Ajout de la logique sur la page Gestion de Produit

--- frontend/app/admin/produits/page.jsx
+++ frontend/app/admin/produits/page.jsx
@@ -1,8 +1,7 @@
 import styles from "./produits.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
-import Image from "next/image";
-import Link from "next/link";
+import ProductCard from '../../../components/ProductCard/ProductCard';
 
 export default async function page() {
   const productRes = await fetch("http://localhost:8080/api/product/", {
@@ -19,33 +18,7 @@ export default async function page() {
       {products ? (
         <section>
           {products.map((product, index) => (
-            <article key={index}>
-              {/* <Link href={`/admin/produits/${product.id}`}> */}
-              {product.Product_Photos.length > 0 && (
-                <div className={styles.imgContainer}>
-                  <Image
-                    className={styles.photo}
-                    src={`http://localhost:8080/api/images/${product.Product_Photos[0].name}`}
-                    width={400}
-                    height={400}
-                    alt={`Photo de ${product.name}`}
-                  />
-                </div>
-              )}
-              <div className={styles.filter}></div>
-              <div className={styles.command}>
-                <div className={styles.infos}>
-                  <h3>{product.name}</h3>
-                  <input
-                    type="checkbox"
-                    id="active"
-                    checked={product.isActive}
-                  />
-                </div>
-                <button className={styles.modifyBtn}>Modifier</button>
-              </div>
-              {/* </Link> */}
-            </article>
+            <ProductCard key={index} product={product} />        
           ))}
         </section>
       ) : (
---
+++ frontend/components/ProductCard/ProductCard.jsx
@@ -0,0 +1,50 @@
+"use client";
+import Image from "next/image";
+import styles from "./ProductCard.module.scss";
+import { useState, useEffect } from "react";
+import Link from "next/link";
+
+export default function ProductCard({ key, product }) {
+  const [status, setStatus] = useState(product.isActive);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/product/modify-product", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ ...product, isActive: status }),
+    })
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  }, [status]);
+
+  return (
+    <article className={styles.productCard} key={key}>
+      {product.Product_Photos.length > 0 && (
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.photo}
+            src={`http://localhost:8080/api/images/${product.Product_Photos[0].name}`}
+            width={400}
+            height={400}
+            alt={`Photo de ${product.name}`}
+          />
+        </div>
+      )}
+      <div className={styles.filter}></div>
+      <div className={styles.command}>
+        <div className={styles.infos}>
+          <h3>{product.name}</h3>
+          <input
+            type="checkbox"
+            id="active"
+            checked={status}
+            onChange={(e) => setStatus(e.target.checked)}
+          />
+        </div>
+        <Link href={`/admin/product/${product.id}`} className={styles.modifyBtn}>Modifier</Link>
+      </div>
+    </article>
+  );
+}
