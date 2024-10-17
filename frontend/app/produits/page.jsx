df38d83663cb71b455b253d5d2554bca9a0c0a7c
Valentin Monteiro <valentin.monteiro.pro@gmail.com>
2024-10-17 18:41:16 +0200

    Ajustement CSS de la page produits (front-office)

--- frontend/app/produits/page.jsx
+++ frontend/app/produits/page.jsx
@@ -2,7 +2,7 @@ import styles from "./produits.module.scss";
 import Image from "next/image";
 import Link from "next/link";
 
-export default async function page() {
+export default async function produits() {
   const productRes = await fetch("http://localhost:8080/api/product/active", {
     method: "POST",
   });
@@ -14,20 +14,18 @@ export default async function page() {
       {products.length > 0 ? (
         <section>
           {products.map((product, index) => (
-            <article key={index}>
-              <Link href={`/produits/${product.id}`}>
+            <Link className={styles.product} key={index} href={`/produits/${product.id}`}>
               <Image
                 className={styles.photo}
                 src={`http://localhost:8080/api/images/${product.Product_Photos[0].name}`}
                 width={400}
                 height={400}
                 alt={`Photo de ${product.name}`}
-                />
+              />
               <div className={styles.hover}>
                 <h3>{product.name}</h3>
               </div>
-                </Link>
-            </article>
+            </Link>
           ))}
         </section>
       ) : (
